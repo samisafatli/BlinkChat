b0500ac4b16cda84c41e92762bd573c5e5ef58da
Sami Safatli <sami.safatli@yduqs.com.br>
2024-09-17 15:36:04 -0300

    Rendering the MainBox only after the user is Authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
-import styles from "./page.module.css";
+'use client'
+
 import { Container } from "@mui/material";
 import Header from "./Components/Header/Header";
 import MainBox from "./Components/MainBox/MainBox";
+import SignInScreen from "./auth/auth";
+import { auth } from "./firebase/clientApp"
+import { useAuthState } from "react-firebase-hooks/auth"
 
 export default function Home() {
+
+  const [user, loading, error] = useAuthState(auth)
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  console.log('user', user)
   return (
     <main>
       <Container>
         <Header></Header>
-        <MainBox></MainBox>
+        {user ? <MainBox /> : <SignInScreen />}
       </Container>
     </main>
   );
